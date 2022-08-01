0c8128413529b741ac9e0978d58e757df47f75cb
Mayya Markova <mayya.markova@gmail.com>
2022-08-01 16:23:02 +0300

    Add styles

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import PersonalInfo from "./components/PersonalInfo";
+import Styles from "./components/Styles";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={Styles.container}>
       <PersonalInfo />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- components/PersonalInfo.tsx
+++ components/PersonalInfo.tsx
@@ -1,14 +1,18 @@
 import { Button, Image, Text, TextInput, View } from "react-native";
 import React from "react";
+import Styles from "./Styles";
 
 const PersonalInfo = () => {
   return (
-    <View>
-      <Image source={require("../assets/wired-brain-coffee-logo.png")} />
+    <View style={Styles.personalInfoContainer}>
+      <Image
+        style={Styles.logo}
+        source={require("../assets/wired-brain-coffee-logo.png")}
+      />
 
-      <View>
-        <Text>Please enter your name..</Text>
-        <TextInput />
+      <View style={Styles.enterYourName}>
+        <Text style={Styles.nameText}>Please enter your name..</Text>
+        <TextInput style={Styles.nameTextInput} />
       </View>
 
       <Button title="Start chatting!" onPress={() => {}} />
