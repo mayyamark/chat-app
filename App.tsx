52124b088a7fe91fa2db8ee4e05c4ade39979da5
Mayya Markova <mayya.markova@gmail.com>
2022-08-02 12:02:24 +0300

    Add AsyncStorage and SplashScreen

--- App.tsx
+++ App.tsx
@@ -2,24 +2,62 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native";
 import PersonalInfo from "./components/PersonalInfo";
 import Styles from "./components/Styles";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Chat from "./components/Chat";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SplashScreen from "expo-splash-screen";
+
+export const storageUsernameKey = "chatapp-username";
+export const storageImageKey = "chatapp-image";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [image, setImage] = useState("");
+  const [isAppReady, setIsAppReady] = useState(true);
 
-  const handlePressStartChatting = (name: string, image: string) => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const fetchedImage = await AsyncStorage.getItem(storageImageKey);
+        const image = fetchedImage ? fetchedImage : "";
+
+        const fetchedUsername = await AsyncStorage.getItem(storageUsernameKey);
+        const username = fetchedUsername ? fetchedUsername : "";
+
+        setImage(image);
+        setUsername(username);
+      } catch (err) {
+        console.warn(err);
+      } finally {
+        setIsAppReady(true);
+      }
+    })();
+  }, []);
+
+  const handlePress = async (name: string, image: string) => {
+    await AsyncStorage.setItem(storageImageKey, image);
+    await AsyncStorage.setItem(storageUsernameKey, username);
+
     setImage(image);
     setUsername(name);
   };
 
   return (
-    <SafeAreaView style={Styles.container}>
+    <SafeAreaView
+      style={Styles.container}
+      onLayout={async () => {
+        if (isAppReady) {
+          await SplashScreen.hideAsync();
+        }
+      }}
+    >
       {username ? (
         <Chat username={username} image={image} />
       ) : (
-        <PersonalInfo onPress={handlePressStartChatting} />
+        <PersonalInfo onPress={handlePress} />
       )}
       <StatusBar style="auto" />
     </SafeAreaView>
